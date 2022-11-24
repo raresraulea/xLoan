76714854f5c5ed17e8d0fead2df3c6ee5755b1c6
Rares Raulea <rares@graffino.com>
2022-11-25 00:57:23 +0200

    feat(deployPool): deploy contract simulation

--- src/components/Layout/Modals/BaseModalExtensions/DeployNewPoolModal.tsx
+++ src/components/Layout/Modals/BaseModalExtensions/DeployNewPoolModal.tsx
@@ -1,16 +1,26 @@
 import React, { useCallback } from 'react';
 import { useGetNetworkConfig } from '@elrondnetwork/dapp-core/hooks';
-import { ProxyNetworkProvider } from '@elrondnetwork/erdjs-network-providers';
+import { sendTransactions } from '@elrondnetwork/dapp-core/services';
+import {
+  getAccount,
+  getAddress,
+  getChainID
+} from '@elrondnetwork/dapp-core/utils';
 import {
   Address,
   AddressValue,
   Code,
   CodeMetadata,
+  DeployArguments,
   ResultsParser,
   SmartContract,
   TransactionWatcher,
+  TypedValue,
   U8Value
-} from '@elrondnetwork/erdjs/out';
+} from '@elrondnetwork/erdjs';
+import { ApiNetworkProvider } from '@elrondnetwork/erdjs-network-providers';
+
+import { Balance } from '@mui/icons-material';
 import { Button, Grid } from '@mui/material';
 import { smartContractCode } from 'helpers/constants';
 
@@ -18,50 +28,101 @@ const DeployNewPoolModal = () => {
   const { network } = useGetNetworkConfig();
 
   console.log({ network });
-  const handleDeployNewPool = useCallback(async () => {
-    console.log('Deploying new contract!');
+  // const handleDeployNewPool = useCallback(async () => {
+  //   console.log('Deploying new contract!');
 
-    const buffer = Buffer.from(smartContractCode);
-    const code = Code.fromBuffer(buffer);
+  //   const buffer = Buffer.from(smartContractCode);
+  //   const code = Code.fromBuffer(buffer);
 
-    const contract = new SmartContract({});
-    const quorumTyped = new U8Value(1);
-    const boardMembers = [
-      new AddressValue(
-        new Address(
-          'erd19rh30cq9964an8vj7qnj7gwaus90nv6020vxpu69ramwrn8yr78smteycl'
-        )
-      )
-    ];
+  //   const contract = new SmartContract({});
+  //   const quorumTyped = new U8Value(1);
+  //   const boardMembers = [
+  //     new AddressValue(
+  //       new Address(
+  //         'erd19rh30cq9964an8vj7qnj7gwaus90nv6020vxpu69ramwrn8yr78smteycl'
+  //       )
+  //     )
+  //   ];
 
-    const transaction = contract.deploy({
-      code: code,
-      codeMetadata: new CodeMetadata(false, true, true),
-      initArguments: [quorumTyped, ...boardMembers],
-      gasLimit: 20000000,
-      chainID: 'T'
-    });
+  //   const transaction = contract.deploy({
+  //     code: code,
+  //     codeMetadata: new CodeMetadata(false, true, true),
+  //     initArguments: [quorumTyped, ...boardMembers],
+  //     gasLimit: 200000000000,
+  //     chainID: 'D'
+  //   });
 
-    const contractAddress = SmartContract.computeAddress(
-      transaction.getSender(),
-      transaction.getNonce()
-    );
+  //   const contractAddress = SmartContract.computeAddress(
+  //     transaction.getSender(),
+  //     transaction.getNonce()
+  //   );
 
-    contract.setAddress(contractAddress);
+  //   contract.setAddress(contractAddress);
 
-    const networkProvider = new ProxyNetworkProvider(network.apiAddress);
+  //   console.log(network.apiAddress);
 
-    await networkProvider.sendTransaction(transaction);
-    const transactionOnNetwork = await new TransactionWatcher(
-      networkProvider
-    ).awaitCompleted(transaction);
+  //   // const networkProvider = new ProxyNetworkProvider(network.apiAddress);
+  //   const networkProvider = new ApiNetworkProvider(network.apiAddress);
 
-    const { returnCode } = new ResultsParser().parseUntypedOutcome(
-      transactionOnNetwork
-    );
+  //   const transactions = [transaction];
+  //   const { sessionId, error } = await sendTransactions({
+  //     transactions
+  //   });
 
-    console.log({ returnCode });
-  }, []);
+  //   const { returnCode } = new ResultsParser().parseUntypedOutcome(
+  //     transactionOnNetwork
+  //   );
+
+  //   console.log({ returnCode });
+  // }, []);
+
+  const handleDeployNewPool = async () => {
+    function getDeployContractTransaction(
+      quorum: number,
+      boardMembers: AddressValue[]
+    ) {
+      const contract = new SmartContract({});
+      const code = Code.fromBuffer(Buffer.from(smartContractCode, 'hex'));
+      const codeMetadata = new CodeMetadata(false, true, true);
+      const quorumTyped = new U8Value(quorum);
+      const initArguments: TypedValue[] = [quorumTyped, ...boardMembers];
+      const value = '0';
+      const deployArguments: DeployArguments = {
+        code,
+        codeMetadata,
+        initArguments,
+        value,
+        gasLimit: 600000000,
+        chainID: 'D'
+      };
+      return contract.deploy(deployArguments);
+    }
+    try {
+      const address = await getAddress();
+      const account = await getAccount(address);
+
+      const multisigAddress = SmartContract.computeAddress(
+        new Address(address),
+        account?.nonce as any
+      );
+      const boardMembers = [new AddressValue(new Address(address))];
+      const quorum = 1;
+      const deployTransaction = getDeployContractTransaction(
+        quorum,
+        boardMembers
+      );
+
+      const transactions = [deployTransaction];
+      const { sessionId, error } = await sendTransactions({
+        transactions
+      });
+
+      return { sessionId, multisigAddress: multisigAddress.bech32(), error };
+    } catch (err) {
+      throw Error('Error deploying safe!');
+    }
+  };
+
   return (
     <Grid container>
       <Grid item xs={12}>
