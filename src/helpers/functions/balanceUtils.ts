093f4da1e94038700e05270e5961afb053f95368
Rares Raulea <rares@graffino.com>
2022-11-25 23:27:30 +0200

    chore: restyle helpers folder structure and add some helpers

---
+++ src/helpers/functions/balanceUtils.ts
@@ -0,0 +1,28 @@
+import { TokenPayment } from '@elrondnetwork/erdjs/out';
+
+export interface IDenominatedBalanceConfig {
+  precisionAfterComma?: number;
+  needsDenomination?: boolean;
+}
+
+export const getDenominatedBalance = <T extends string | number>(
+  stringBalance: string,
+  {
+    precisionAfterComma = 4,
+    needsDenomination = true
+  }: IDenominatedBalanceConfig
+): T => {
+  const balanceAfterDenomination = parseFloat(
+    needsDenomination
+      ? TokenPayment.egldFromBigInteger(stringBalance).toPrettyString()
+      : stringBalance
+  );
+  const balanceFloor = Math.floor(balanceAfterDenomination);
+  const balanceSubunit = balanceAfterDenomination - balanceFloor;
+  const balanceSubunitAfterPrecision = parseFloat(
+    balanceSubunit.toFixed(precisionAfterComma)
+  );
+  const denominatedBalance = balanceFloor + balanceSubunitAfterPrecision;
+
+  return denominatedBalance as T;
+};
