409065a68b6a2c12d9575afa9bff7ff1f6ecc42a
Rares Raulea <rares@graffino.com>
2022-11-26 01:32:34 +0200

    feat(loanPools): add inidividual provider presentation component

---
+++ src/components/Forms/CreateLoanPoolForm/ProviderPresentation.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { IdentityWithColumns } from 'interfaces';
+import APRColumn from './APRColumn';
+import FilledColumn from './FilledColumn';
+import ProviderColumn from './ProviderColumn';
+
+interface Props {
+  provider?: IdentityWithColumns;
+}
+
+const ProviderPresentation = ({ provider }: Props) => {
+  if (!provider) {
+    return <div>No provider to show</div>;
+  }
+  return (
+    <Box
+      height={68}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}
+    >
+      <ProviderColumn columnData={provider.providerColumn} />
+      <APRColumn columnData={provider.aprColumn} />
+      <FilledColumn columnData={provider.filledColumn} />
+    </Box>
+  );
+};
+
+export default ProviderPresentation;
